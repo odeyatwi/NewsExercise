b38666b50f527a3dff8803edba13b0b17fa7ecb3
Odeya Twito <odeya.twito@bankleumi.co.il>
2022-04-09 22:15:17 +0300

    fix api responses types

--- src/@types/response.ts
+++ src/@types/response.ts
@@ -1,4 +1,6 @@
-export type NewsResponseItem = {
+type Status = 'ok' | 'error';
+
+export type SourceResponseItem = {
   id: string;
   name: string;
   description: string;
@@ -8,7 +10,27 @@ export type NewsResponseItem = {
   country: string;
 };
 
+export type SourcesResponse = {
+  status: Status;
+  sources: SourceResponseItem[];
+};
+
+export type NewsResponseItem = {
+  source: {
+    id: string;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string;
+  publishedAt: string;
+  content: string;
+};
+
 export type NewsResponse = {
-  status: string;
-  sources: NewsResponseItem[];
+  status: Status;
+  totalResults: number;
+  articles: NewsResponseItem[];
 };
